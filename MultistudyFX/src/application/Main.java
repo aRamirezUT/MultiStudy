98ae146227185ab3e70b2edf73cdc8638ea70bc9
Hamja291 <99988739+Hamja291@users.noreply.github.com>
2022-04-26 21:00:37 -0500

    Notepad

--- MultistudyFX/src/application/Main.java
+++ MultistudyFX/src/application/Main.java
@@ -1,6 +1,5 @@
 package application;
-    
-import application.Model.Timer;
+	
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -9,25 +8,21 @@ import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
-	public static Timer t;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,1000,1000);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("NOTEPAD");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/application/view/Main.fxml"));
-            Scene scene = new Scene(root,400,400);
-            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("Multistudy");
-            primaryStage.show();
-    
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public static void main(String[] args) {
-    	t = new Timer();
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
