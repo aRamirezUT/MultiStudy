5f2a229153e978930772e3498d45824db3de15a8
aRamirezUT <99777095+aRamirezUT@users.noreply.github.com>
2022-04-26 21:06:31 -0500

    Update NotepadController.java

--- MultistudyFX/src/application/controller/NotepadController.java
+++ MultistudyFX/src/application/controller/NotepadController.java
@@ -1,25 +1,60 @@
-package application.controller;
+package application;
 
-import java.io.IOException;
+import java.awt.TextField;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
-import javafx.event.ActionEvent;
+import javax.swing.DefaultListModel;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
 
-public class NotepadController {
-    @FXML
-    private BorderPane notepadPane; 
-    @FXML
-    void homeHandle(ActionEvent event) throws IOException {
-    	notepadPane = FXMLLoader.load(getClass().getResource("/application/view/Main.fxml"));// going to Main Pane
-    	Scene scene = new Scene(notepadPane);// pane you are GOING TO show
-    	Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow() ;// pane you are ON
-    	window.setScene(scene);
-    	window.show();
-    }
+public class SampleController implements Initializable {
+	ObservableList<String> savedNotes = FXCollections.observableArrayList();
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");  
+	LocalDateTime now = LocalDateTime.now(); 
+	 @FXML
+	ListView notes ;
+	@FXML
+	Button addNote;
+	@FXML
+	TextArea note;
+	@FXML
+	Text date;
+	
+	
+	@FXML
+	private void addNote(Event e) {
+		
+		if(note.getText().length()>0) {
+			
+			
+		savedNotes.add(note.getText().toString());
+		notes.getItems().add(note.getText());
 
+		
+		
+		}
+		
+		
+		
+	}
+	
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		System.out.print(savedNotes);
+		date.setText(dtf.format(now));
+		
+		
+	}
 }
